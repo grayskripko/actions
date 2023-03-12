811dc3462f8cebbceb781d204f741f4933d9db35
Gray <grayskripko@gmail.com>
2023-03-12 15:50:43 -0300

    Update fetch_rss.py

--- fetch_rss.py
+++ fetch_rss.py
@@ -2,11 +2,14 @@ import feedparser
 import os
 import datetime
 import telegram
+import html
 from asyncio import run
     
 
 async def send_message(bot, chat_id, message):
-    await bot.send_message(chat_id=chat_id, text=message)
+    await bot.send_message(
+        chat_id=chat_id, text=html.escape(message),
+        parse_mode=telegram.ParseMode.HTML)
     
 def main():
     rss_feed_url = os.getenv('RSS_FEED_URL')
@@ -17,11 +20,15 @@ def main():
 #     update_freq = 30 * 60  # test
     feed = feedparser.parse(rss_feed_url)
     
+    run(send_message(
+        bot, chat_id, 'Tutoring In R ' +
+        'I just need about an hour of tutoring. <br /><br /><b>Hourly Range</b>: $20.00-$40.00'))
+    
     for entry in feed.entries:
         published_datetime = datetime.datetime.strptime(entry.published, '%a, %d %b %Y %H:%M:%S %z')
         time_difference = datetime.datetime.now(datetime.timezone.utc) - published_datetime
         is_obsolete = time_difference > datetime.timedelta(minutes=update_freq)
-
+        
         if not is_obsolete:
 #             print(entry)
             ttl = entry.title.replace(" - Upwork", "")
@@ -29,9 +36,6 @@ def main():
             print(message)
             bot = telegram.Bot(token=bot_token)
             run(send_message(bot, chat_id, message))
-#             send_text = f'https://api.telegram.org/bot{bot_token}/sendMessage?chat_id=' + \
-#                 f'{chat_id}&parse_mode=Markdown&text={message}'
-#             response = requests.get(send_text)
             
 
 if __name__ == '__main__':
