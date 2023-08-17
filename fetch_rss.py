56b6d04b0dde094c051fbcc6a0fd6f5c8ee1713b
grayskripko <grayskripko@gmail.com>
2023-08-17 12:49:07 -0300

    lap

--- fetch_rss.py
+++ fetch_rss.py
@@ -7,6 +7,14 @@ import re
 from asyncio import run
     
 
+def get_url():
+    prv = os.getenv('UPWORKER_PRV')
+    return 'https://www.upwork.com/ab/feed/jobs/rss?' + prv +\
+        '&api_params=1&contractor_tier=2,3&paging=0;10&sort=recency&verified_payment_only=1' +\
+        '&hourly_rate=50-&job_type=hourly&proposals=0-4,5-9,10-14,15-19&q=' +\
+        '("google analytics" OR GA OR airtable) OR ' +\
+        'skills:(excel OR airtable OR sql OR "data analytics" OR "data analysis")'
+
 async def send_message(bot, chat_id, message):
     await bot.send_message(
         chat_id=chat_id, text=html.unescape(
@@ -14,14 +22,13 @@ async def send_message(bot, chat_id, message):
         parse_mode=telegram.constants.ParseMode.HTML)
     
 def main():
-    # https://www.upwork.com/ab/feed/topics/rss?securityToken={}&userUid={}&orgUid={}&topic={}
-    rss_feed_url = os.getenv('RSS_FEED_URL')
     bot_token = os.getenv('TELEGRAM_TOKEN')
     chat_id = os.getenv('TELEGRAM_TO')
-    update_freq = 15.5
-    #min_hourly = 25  # dollars per hour
+
+    update_freq = 10.5
+    min_hourly_salary = 25
     
-    feed = feedparser.parse(rss_feed_url)
+    feed = feedparser.parse(get_url())
     
     for i, entry in enumerate(feed.entries):
         if i == 0:
@@ -31,9 +38,9 @@ def main():
         is_obsolete = time_difference > datetime.timedelta(minutes=update_freq)
         
         if not is_obsolete:
-            #min_hourly_regx = re.search(r'Hourly Range</b>: \$([^\.-]+)', entry['summary'])
-            #if min_hourly_regx and int(min_hourly_regx[1]) < min_hourly:
-            #    continue
+            min_hourly_regx = re.search(r'Hourly Range</b>: \$([^\.-]+)', entry['summary'])
+            if min_hourly_regx and int(min_hourly_regx[1]) < min_hourly_salary:
+               continue
             ttl = '<b>' + entry.title.replace(" - Upwork", "") + '</b>'
             message = f'{ttl}\n{entry.summary}'
             print(message)
---
+++ script.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+import pandas as pd
+import feedparser
+from urllib.parse import quote
+from fetch_rss import get_url
+
+
+def print_recent_gigs(url):
+    rss_feed_url = quote(url, safe=':/&=?')
+    feed = feedparser.parse(rss_feed_url)
+    res = pd.DataFrame(feed['entries'])\
+        .assign(date = lambda d: d['published'].apply(
+            lambda x: datetime.strptime(x, '%a, %d %b %Y %H:%M:%S %z').strftime('%a %H:%M')),\
+                title = lambda d: d['title'].replace({'&amp;': '&'}, regex=True))\
+        [['date', 'title', 'summary']]\
+        .sort_values('date', ascending=False)\
+        .set_index('date')
+    print(res.head())
+
+urls = [
+    "https://www.upwork.com/ab/feed/topics/rss?securityToken=6351f1c6fbe3477ad3fe521ee0b841899d795fac367917dbff3cc2064e4f0c6b2496f884f4441ad10f97ca6f0db3c8c83f838f65f9b9e73cbb27a159b5662b25&userUid=551492515965923328&orgUid=551492515965923330&topic=6734355",
+    get_url()    
+]
+
+[print_recent_gigs(u) for u in urls]
