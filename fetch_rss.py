4097c3762e4b53817fa779f7b7bfecb961033ea7
gray <grayskripko@gmail.com>
2023-08-23 15:22:18 -0300

    upd

--- fetch_rss.py
+++ fetch_rss.py
@@ -8,10 +8,10 @@ from asyncio import run
 def get_url():
     prv = os.getenv('UPWORKER_PRV')
     add_excel = 'OR "google sheets" OR excel OR airtable' if True else ''
-    url = 'https://www.upwork.com/ab/feed/jobs/rss?' + prv +\
-        '&api_params=1&contractor_tier=2,3&paging=0;10&sort=recency&verified_payment_only=1&' +\
+    url = f'https://www.upwork.com/ab/feed/jobs/rss?{prv}&' +\
+        'api_params=1&contractor_tier=2,3&paging=0;10&sort=recency&verified_payment_only=1&' +\
         'job_type=hourly,fixed&budget=300-&hourly_rate=30-&q=' +\
-        f'skills:(dax OR spark OR database R OR sql OR etl OR dashboard OR dash OR "data analytics" OR "data analysis" {add_excel})'
+        f'skills:(R OR sql OR etl OR dashboard OR dash OR "data analytics" OR "data analysis" {add_excel})'
     print(url)
     return quote(url, safe=':/&=?')
 
@@ -36,16 +36,16 @@ def main():
         published_datetime = datetime.strptime(entry.published, '%a, %d %b %Y %H:%M:%S %z')
         time_difference = datetime.now(timezone.utc) - published_datetime
         if time_difference > timedelta(seconds=update_freq):
-            print(f'Obsolete [{ttl}]: {time_difference}, {datetime.now(timezone.utc)}, {published_datetime}')
+            print(f'- Old [{ttl}]: {time_difference}, {datetime.now(timezone.utc)}, {published_datetime}')
             continue
 
         min_hourly_regx = re.search(r'Hourly Range</b>: \$([^\.-]+)', entry['summary'])
         if min_hourly_regx and int(min_hourly_regx[1]) < min_hourly_salary:
-            print(f'Cheap [{ttl}]: {min_hourly_regx[1]}')
+            print(f'- Cheap [{ttl}]: {min_hourly_regx[1]}')
             continue
 
         message = f'{ttl}\n{entry.summary}'
-        print(f'Tg [{ttl}]')
+        print(f'+ Send [{ttl}]')
         bot = telegram.Bot(token=bot_token)
         run(send_message(bot, chat_id, message))
             
