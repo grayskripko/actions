fad6aa41ff4448e2ff559f8197e10352061a604d
gray <grayskripko@gmail.com>
2023-08-23 12:50:59 -0300

    lap

--- fetch_rss.py
+++ fetch_rss.py
@@ -8,8 +8,8 @@ from asyncio import run
 def get_url():
     prv = os.getenv('UPWORKER_PRV')
     add_excel = 'OR "google sheets" OR excel OR airtable' if True else ''
-    url = 'https://www.upwork.com/ab/feed/jobs/rss?&' + prv +\
-        'api_params=1&contractor_tier=2,3&paging=0;10&sort=recency&verified_payment_only=1&' +\
+    url = 'https://www.upwork.com/ab/feed/jobs/rss?' + prv +\
+        '&api_params=1&contractor_tier=2,3&paging=0;10&sort=recency&verified_payment_only=1&' +\
         'job_type=hourly,fixed&budget=300-&hourly_rate=30-&q=' +\
         f'skills:(dax OR spark OR database R OR sql OR etl OR dashboard OR dash OR "data analytics" OR "data analysis" {add_excel})'
     print(url)
